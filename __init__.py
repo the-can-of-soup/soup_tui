582ddfa657bce42e04da4f01ef52e76a68cd1150
the-can-of-soup <campbell+github@campbellkeller.net>
2025-03-23 12:10:21 -0400

    Add note to __init__.py docstring

--- __init__.py
+++ __init__.py
@@ -1,5 +1,10 @@
 """
 A small module written by Soup for basic text UI and user input.
+
+RUN THE FOLLOWING FOR FULL DOCUMENTATION:
+
+import soup_tui.soup_tui
+help(soup_tui.soup_tui)
 """
 
 from .soup_tui import *
